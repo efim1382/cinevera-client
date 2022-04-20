2a37f21aa3eb3e8568a7a4da3fdd48e70f211f9d
Roman <efim1382@gmail.com>
2022-04-20 17:58:05 +0300

    not found page

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import Icon from "components/Icon";
+import BasicButton from "components/BasicButton";
+import style from "./style.css";
 
 const NotFound = () => {
 	return (
-		<div>
-			Not Found
-		</div>
+		<main className={style.not_found}>
+			<div className="container">
+				<section className={style.section}>
+					<h1>404</h1>
+					<p className={style.description}>Sorry, the page you are<br />looking for could be found</p>
+
+					<NavLink to="/" className={style.link}>
+						<BasicButton
+							theme="contrast"
+							text="Go Home"
+						/>
+					</NavLink>
+				</section>
+
+				<div className={style.illustration}>
+					<Icon name="question" />
+				</div>
+			</div>
+		</main>
 	);
 };
 
