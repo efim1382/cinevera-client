1c268ff6f92a8ad0fbf81a6d21a30ac9656ff148
kottbart <yuriikorbat@gmail.com>
2022-04-20 23:32:36 +0300

    some fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,17 +4,17 @@ import style from "./style.css";
 import Icon from "components/Icon";
 
 const Header = () => {
-  return (
-    <header className={style.header}>
-      <NavLink to="/" className={style.logo}>
-        <h1>SearchFilm</h1>
+	return (
+		<header className={style.header}>
+			<NavLink to="/" className={style.logo}>
+				<h1>SearchFilm</h1>
       </NavLink>
-      
-      <button className={style.profile_button}>
-        <Icon name="account_circle" className={style.account_circle} />
-      </button>
-    </header>
-  );
+
+			<button className={style.profile_button}>
+				<Icon name="account_circle" className={style.account_circle} />
+			</button>
+		</header>
+	);
 };
 
 export default Header;
