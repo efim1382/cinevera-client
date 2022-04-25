11b48b911d3db9bfd9de353cd705a1caba0c0d4d
Roman <efim1382@gmail.com>
2022-04-26 00:22:33 +0300

    temp changes

--- src/@master/containers/Home/Home.jsx
+++ src/@master/containers/Home/Home.jsx
@@ -1,9 +1,64 @@
 import React from "react";
+import Header from "@master/containers/FilmDetails/Header";
+import style from "./style.css";
 
 const Home = () => {
 	return (
-		<div>
-			Home
+		<div className={style.home}>
+			<Header />
+
+			<section className={style.main_block}>
+				<div className="container">
+					<h1 className={style.title}>The Queen's<br />Gambit</h1>
+
+					<p className={style.description}>
+						Orphaned at the tender age of nine,
+						<br />
+						prodigious introvert Beth Harmon discovers
+						<br />
+						and masters the game of chess in 1960s USA.
+						<br />
+						But child stardom comes at a price.
+					</p>
+
+					<div className={style.actions}>
+						<button className={style.watch}>Watch</button>
+						<button className={style.trailer}>Play Trailer</button>
+					</div>
+				</div>
+			</section>
+
+			<section className={style.continue}>
+				<div className="container">
+					<h2 className={style.subtitle}>Continue Watching</h2>
+
+					<div className={style.continue_list}>
+						<div className={style.continue_item} />
+						<div className={style.continue_item} />
+						<div className={style.continue_item} />
+						<div className={style.continue_item} />
+						<div className={style.continue_item} />
+						<div className={style.continue_item} />
+						<div className={style.continue_item} />
+					</div>
+				</div>
+			</section>
+
+			<section className={style.popular}>
+				<div className="container">
+					<h2 className={style.subtitle}>Popular Now</h2>
+
+					<div className={style.popular_list}>
+						<div className={style.popular_item} />
+						<div className={style.popular_item} />
+						<div className={style.popular_item} />
+						<div className={style.popular_item} />
+						<div className={style.popular_item} />
+						<div className={style.popular_item} />
+						<div className={style.popular_item} />
+					</div>
+				</div>
+			</section>
 		</div>
 	);
 };
