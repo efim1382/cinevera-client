8ec856467108bc9b0ea8fd1f9d0eb17564a59e87
Roman <efim1382@gmail.com>
2022-04-26 20:31:55 +0300

    some fixes

--- src/@master/containers/Home/Home.jsx
+++ src/@master/containers/Home/Home.jsx
@@ -1,90 +1,7 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import Header from "@master/containers/FilmDetails/Header";
 import style from "./style.css";
-
-const continueItems = [
-	{
-		_id: 1,
-		image: "url(https://wallpapershome.ru/images/pages/pic_v/12849.jpg)",
-	},
-
-	{
-		_id: 2,
-		image: "url(https://filmix.ac/uploads/frames/118698/f247290-vystrel-v-pustotu_2017_original.jpg)",
-	},
-
-	{
-		_id: 3,
-		image: "url(https://cdn.oboi7.com/content/images/e3/ee/e3ee9a3e26ec84cae113de17a43691af853ab0fe.jpg)",
-	},
-
-	{
-		_id: 4,
-		image: "url(https://p4.wallpaperbetter.com/wallpaper/1024/392/1003/movie-fury-wallpaper-thumb.jpg)",
-	},
-
-	{
-		_id: 5,
-		image: "url(https://wallpapershome.ru/images/pages/pic_v/12849.jpg)",
-	},
-
-	{
-		_id: 6,
-		image: "url(https://wallpapershome.ru/images/pages/pic_v/12849.jpg)",
-	},
-
-	{
-		_id: 7,
-		image: "url(https://cdn.oboi7.com/content/images/e3/ee/e3ee9a3e26ec84cae113de17a43691af853ab0fe.jpg)",
-	},
-
-	{
-		_id: 8,
-		image: "url(https://p4.wallpaperbetter.com/wallpaper/1024/392/1003/movie-fury-wallpaper-thumb.jpg)",
-	},
-];
-
-const popularItems = [
-	{
-		_id: 1,
-		image: "url(https://images.kinorium.com/movie/shot/381127/w1500_1263015.jpg)",
-	},
-
-	{
-		_id: 2,
-		image: "url(https://i.pinimg.com/736x/48/f6/f2/48f6f2113680380e5edb2d378a5f3eac.jpg)",
-	},
-
-	{
-		_id: 3,
-		image: "url(https://www.kinogallery.com/img/wallpaper/kinogallery.com_Inglourious-Basterds-17_1600.jpg)",
-	},
-
-	{
-		_id: 4,
-		image: "url(https://pbs.twimg.com/media/D9rZWPCWsAEeDg-.jpg)",
-	},
-
-	{
-		_id: 5,
-		image: "url(https://i.pinimg.com/736x/79/2a/f2/792af24e261c83b9363b83c21a190474.jpg)",
-	},
-
-	{
-		_id: 6,
-		image: "url(https://pbs.twimg.com/media/D9rZWPCWsAEeDg-.jpg)",
-	},
-
-	{
-		_id: 7,
-		image: "url(https://i.pinimg.com/736x/48/f6/f2/48f6f2113680380e5edb2d378a5f3eac.jpg)",
-	},
-
-	{
-		_id: 8,
-		image: "url(https://images.kinorium.com/movie/shot/381127/w1500_1263015.jpg)",
-	},
-];
 
 const Home = () => {
 	return (
@@ -106,7 +23,7 @@ const Home = () => {
 					</p>
 
 					<div className={style.actions}>
-						<button className={style.watch}>Watch</button>
+						<NavLink to="/film/" className={style.watch}>Watch</NavLink>
 						<button className={style.trailer}>Play Trailer</button>
 					</div>
 				</div>
@@ -148,3 +65,87 @@ const Home = () => {
 };
 
 export default Home;
+
+export const continueItems = [
+	{
+		_id: 1,
+		image: "url(https://wallpapershome.ru/images/pages/pic_v/12849.jpg)",
+	},
+
+	{
+		_id: 2,
+		image: "url(https://filmix.ac/uploads/frames/118698/f247290-vystrel-v-pustotu_2017_original.jpg)",
+	},
+
+	{
+		_id: 3,
+		image: "url(https://cdn.oboi7.com/content/images/e3/ee/e3ee9a3e26ec84cae113de17a43691af853ab0fe.jpg)",
+	},
+
+	{
+		_id: 4,
+		image: "url(https://p4.wallpaperbetter.com/wallpaper/1024/392/1003/movie-fury-wallpaper-thumb.jpg)",
+	},
+
+	{
+		_id: 5,
+		image: "url(https://wallpapershome.ru/images/pages/pic_v/12849.jpg)",
+	},
+
+	{
+		_id: 6,
+		image: "url(https://wallpapershome.ru/images/pages/pic_v/12849.jpg)",
+	},
+
+	{
+		_id: 7,
+		image: "url(https://cdn.oboi7.com/content/images/e3/ee/e3ee9a3e26ec84cae113de17a43691af853ab0fe.jpg)",
+	},
+
+	{
+		_id: 8,
+		image: "url(https://p4.wallpaperbetter.com/wallpaper/1024/392/1003/movie-fury-wallpaper-thumb.jpg)",
+	},
+];
+
+export const popularItems = [
+	{
+		_id: 1,
+		image: "url(https://images.kinorium.com/movie/shot/381127/w1500_1263015.jpg)",
+	},
+
+	{
+		_id: 2,
+		image: "url(https://i.pinimg.com/736x/48/f6/f2/48f6f2113680380e5edb2d378a5f3eac.jpg)",
+	},
+
+	{
+		_id: 3,
+		image: "url(https://www.kinogallery.com/img/wallpaper/kinogallery.com_Inglourious-Basterds-17_1600.jpg)",
+	},
+
+	{
+		_id: 4,
+		image: "url(https://pbs.twimg.com/media/D9rZWPCWsAEeDg-.jpg)",
+	},
+
+	{
+		_id: 5,
+		image: "url(https://i.pinimg.com/736x/79/2a/f2/792af24e261c83b9363b83c21a190474.jpg)",
+	},
+
+	{
+		_id: 6,
+		image: "url(https://pbs.twimg.com/media/D9rZWPCWsAEeDg-.jpg)",
+	},
+
+	{
+		_id: 7,
+		image: "url(https://i.pinimg.com/736x/48/f6/f2/48f6f2113680380e5edb2d378a5f3eac.jpg)",
+	},
+
+	{
+		_id: 8,
+		image: "url(https://images.kinorium.com/movie/shot/381127/w1500_1263015.jpg)",
+	},
+];
