115739d1d3ecf842ad7584ae3c8447fbb4444a7a
Roman <efim1382@gmail.com>
2022-04-28 19:24:39 +0300

    refactore auth

--- src/@master/containers/Auth/SignUp/SignUp.jsx
+++ src/@master/containers/Auth/SignUp/SignUp.jsx
@@ -1,6 +1,5 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
-import Icon from "components/Icon";
 import Form from "components/Form/Form";
 import Input from "components/Form/Input";
 import TextField from "components/FormElements/TextField";
@@ -31,95 +30,66 @@ const SignUp = () => {
 	};
 
 	return (
-		<Fragment>
+		<main className={style.auth}>
 			<header className={style.header}>
-				<NavLink to="/" className={style.logo}>
-					<h1>Cinevera</h1>
-				</NavLink>
+				<p className={style.logo}>cinevera</p>
 			</header>
 
-			<main className={style.auth}>
-				<div className={style.background}>
-					<h2>Welcome to <span>SearchTabs</span></h2>
+			<div className={style.section}>
+				<h2 className={style.title}>Sign up to cinevera</h2>
 
-					<p className={style.text_description}>
-						To keep connected with us please
-						<br />
-						login with your personal info
-					</p>
-
-					<NavLink to="/auth/sign-in/" className={style.link}>
-						<BasicButton
-							appearance="outline"
-							theme="contrast"
-							text="Sign In"
+				<Form
+					className={style.form}
+					onSubmit={onSubmit}
+					validations={validations}
+				>
+					<Input name="name">
+						<TextField
+							label="Name"
+							placeholder="John Wick"
+							icon="person"
+							className={style.input}
 						/>
-					</NavLink>
-				</div>
+					</Input>
 
-				<div className={style.section}>
-					<h2 className={style.subtitle}>Create Account</h2>
-
-					<div className={style.social}>
-						<button type="button">
-							<Icon name="facebook" iconSize="20px" />
-						</button>
-
-						<button type="button">
-							<Icon name="google-plus" iconSize="30px" />
-						</button>
-
-						<button type="button">
-							<Icon name="vk" iconSize="28px" />
-						</button>
-					</div>
-
-					<p className={style.another_text}>Or use your email for registration</p>
-
-					<Form
-						className={style.form}
-						onSubmit={onSubmit}
-						validations={validations}
-					>
-						<Input name="name">
-							<TextField
-								label="Name"
-								placeholder="John Wick"
-								icon="person"
-								className={style.input}
-							/>
-						</Input>
-
-						<Input name="email">
-							<TextField
-								type="email"
-								label="Email"
-								placeholder="someemail@gmail.com"
-								icon="email"
-								className={style.input}
-							/>
-						</Input>
-
-						<Input name="password">
-							<TextField
-								type="password"
-								label="Password"
-								placeholder="********"
-								icon="lock"
-								className={style.input}
-							/>
-						</Input>
-
-						<BasicButton
-							type="submit"
-							text="Sign Up"
-							isLoading={isFetching}
-							className={style.submit}
+					<Input name="email">
+						<TextField
+							type="email"
+							label="Email"
+							placeholder="someemail@gmail.com"
+							icon="email"
+							className={style.input}
 						/>
-					</Form>
-				</div>
-			</main>
-		</Fragment>
+					</Input>
+
+					<Input name="password">
+						<TextField
+							type="password"
+							label="Password"
+							placeholder="********"
+							icon="lock"
+							className={style.input}
+						/>
+					</Input>
+
+					<BasicButton
+						type="submit"
+						text="Sign Up"
+						isLoading={isFetching}
+						className={style.submit}
+					/>
+				</Form>
+			</div>
+
+			<p className={style.auth_link}>
+				Already a member?
+				{" "}
+
+				<NavLink to="/auth/sign-in/">
+					Sign in now
+				</NavLink>
+			</p>
+		</main>
 	);
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,113 +2,113 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const postcssPresetEnv = require('postcss-preset-env');
+const postcssPresetEnv = require("postcss-preset-env");
 
 module.exports = {
-  entry: path.resolve(__dirname, "src/index.js"),
+	entry: path.resolve(__dirname, "src/index.js"),
 
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.js",
-    publicPath: "/",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "[name].bundle.js",
+		publicPath: "/",
 	},
 
-  resolve: {
-    extensions: [
-      ".js",
-      ".jsx",
-      ".json",
-    ],
+	resolve: {
+		extensions: [
+			".js",
+			".jsx",
+			".json",
+		],
 
-    alias: {
-      "@master": `${__dirname}/src/@master`,
-      "@panel": `${__dirname}/src/@panel`,
-      "components": `${__dirname}/src/components`,
-      "compositions": `${__dirname}/src/compositions`,
-      "classes": `${__dirname}/src/classes`,
-      "api": `${__dirname}/src/api`,
-      "actions": `${__dirname}/src/actions`,
-      "selectors": `${__dirname}/src/selectors`,
-      "reducers": `${__dirname}/src/reducers`,
-      "config": `${__dirname}/src/config`,
-      "helpers": `${__dirname}/src/helpers`,
-      "hooks": `${__dirname}/src/hooks`,
-      "store": `${__dirname}/src/store`,
-    },
+		alias: {
+			"@master": `${__dirname}/src/@master`,
+			"@panel": `${__dirname}/src/@panel`,
+			"components": `${__dirname}/src/components`,
+			"compositions": `${__dirname}/src/compositions`,
+			"classes": `${__dirname}/src/classes`,
+			"api": `${__dirname}/src/api`,
+			"actions": `${__dirname}/src/actions`,
+			"selectors": `${__dirname}/src/selectors`,
+			"reducers": `${__dirname}/src/reducers`,
+			"config": `${__dirname}/src/config`,
+			"helpers": `${__dirname}/src/helpers`,
+			"hooks": `${__dirname}/src/hooks`,
+			"store": `${__dirname}/src/store`,
+		},
 
 		modules: [
 			`${__dirname}/node_modules`, "node_modules",
 		],
-  },
+	},
 
-  module: {
-    rules: [
-      {
-        test: /\.js$|\.jsx$/,
-        exclude: /node_modules/,
+	module: {
+		rules: [
+			{
+				test: /\.js$|\.jsx$/,
+				exclude: /node_modules/,
 
-        use: {
-          loader: "babel-loader",
-        },
-      },
+				use: {
+					loader: "babel-loader",
+				},
+			},
 
-      {
-        test: /\.css$/,
+			{
+				test: /\.css$/,
 
-        use: [
-          {
-            loader: "style-loader",
-          },
+				use: [
+					{
+						loader: "style-loader",
+					},
 
-          {
-            loader: "css-loader",
+					{
+						loader: "css-loader",
 
-            options: {
-              modules: {
-                mode: "local",
-                localIdentName: "[local]___[hash:base64:5]",
-              },
-            },
-          },
+						options: {
+							modules: {
+								mode: "local",
+								localIdentName: "[local]___[hash:base64:5]",
+							},
+						},
+					},
 
-          {
-            loader: "postcss-loader",
+					{
+						loader: "postcss-loader",
 
-            options: {
-              postcssOptions: {
-                plugins: [
-                  [
+						options: {
+							postcssOptions: {
+								plugins: [
+									[
 										postcssPresetEnv({
 											features: {
 												"nesting-rules": true,
 											},
 										}),
-                  ],
-                ],
-              },
+									],
+								],
+							},
 
-              sourceMap: true,
-            },
-          },
-        ],
-      },
+							sourceMap: true,
+						},
+					},
+				],
+			},
 
 			{
-        test: /\.(png|jpe?g|gif|ttf|eot|woff|otf|woff2|svg)$/,
-				type: 'asset/resource',
+				test: /\.(png|jpe?g|gif|ttf|eot|woff|otf|woff2|svg)$/,
+				type: "asset/resource",
 			},
-    ],
-  },
+		],
+	},
 
-  plugins: [
-    new CleanWebpackPlugin(),
+	plugins: [
+		new CleanWebpackPlugin(),
 
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src/index.html"),
-    }),
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "src/index.html"),
+		}),
 
-    new webpack.DefinePlugin({
-      "process.env.API_HOST": JSON.stringify(process.env.API_HOST),
-    }),
-  ],
+		new webpack.DefinePlugin({
+			"process.env.API_HOST": JSON.stringify(process.env.API_HOST),
+		}),
+	],
 };
