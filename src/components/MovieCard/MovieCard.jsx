4ffc6fa16a35480ea34b6d7810fdc4b2f33b3860
kottbart <yuriikorbat@gmail.com>
2022-05-02 00:01:42 +0300

    will made hover to moviecard

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { NavLink } from "react-router-dom";
 import classnames from "classnames/bind";
+import Icon from "components/Icon";
 import style from "./style.css";
 
 const cx = classnames.bind(style);
@@ -16,7 +17,20 @@ const MovieCard = (props) => {
 	const inline = { backgroundImage: `url("${image}")` };
 
 	return (
-		<NavLink to={link} className={cx("movie_card", className)} style={inline} />
+		<NavLink to={link} className={cx("movie_card", className)} style={inline}>
+			<div className={style.description_film}>
+				<p className={ style.name_film}>Fight Club</p>
+				<button className={style.button_star}>
+					<Icon name="star" />
+				</button>
+				<div className={style.film_info}>
+					<span className={style.film_year}>1997</span>
+					<span className={style.film_genre}>Thriller</span>
+					<span className={style.film_duration}>139</span>
+				</div>
+			</div>
+
+		</NavLink>
 	);
 };
 
