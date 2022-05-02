b9c7a8f39ff52bf6e44a14fc1d591fb002e80990
kottbart <yuriikorbat@gmail.com>
2022-05-02 18:51:45 +0300

    add hover to movieCard

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import { NavLink } from "react-router-dom";
 import classnames from "classnames/bind";
-import Icon from "components/Icon";
 import style from "./style.css";
 
 const cx = classnames.bind(style);
@@ -18,18 +17,23 @@ const MovieCard = (props) => {
 
 	return (
 		<NavLink to={link} className={cx("movie_card", className)} style={inline}>
-			<div className={style.description_film}>
-				<p className={ style.name_film}>Fight Club</p>
-				<button className={style.button_star}>
-					<Icon name="star" />
-				</button>
-				<div className={style.film_info}>
-					<span className={style.film_year}>1997</span>
-					<span className={style.film_genre}>Thriller</span>
-					<span className={style.film_duration}>139</span>
-				</div>
-			</div>
+			<div className={style.content}>
+				<p className={style.name}>Fight Club</p>
 
+				<div className={style.score_film}>
+					<p className={style.rating}>9.3</p>
+					<div className={style.lane_box}>
+						<div className={style.score_line} />
+					</div>
+				</div>
+
+				<p className={style.description}>
+					"The first rule of Fight Club is: you do not talk about Fight Club.
+					 The second rule of Fight Club is: you DO NOT talk about Fight Club!" - Tyler Durden
+				</p>
+
+				<p className={style.film_info}>1997 year 139 min<br />Thriller</p>
+			</div>
 		</NavLink>
 	);
 };
