9fd1cabf8e3ccd4ccceee028eb32dfd35acebf9f
kottbart <yuriikorbat@gmail.com>
2022-05-04 01:56:03 +0300

    add series container

---
+++ src/@master/containers/Series/Series.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./style.css";
+
+const Series = () => {
+	return (
+		<main className={style.series}>
+			series
+		</main>
+	);
+};
+
+export default Series;
