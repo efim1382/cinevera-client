c5f546542a2a4185e29c4e55065f521d760457c4
kottbart <yuriikorbat@gmail.com>
2022-05-09 20:22:06 +0300

    filled the overlay

--- src/@master/containers/SearchOverlay/SearchOverlay.jsx
+++ src/@master/containers/SearchOverlay/SearchOverlay.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import useQuery from "hooks/useQuery";
 import style from "./style.css";
+import Icon from "components/Icon";
 
 export const queryParam = "search";
 
@@ -22,8 +23,65 @@ const SearchOverlay = () => {
 				onClick={closeOverlay}
 				className={style.close_button}
 			>
-				close
+				<Icon name="close" />
 			</button>
+
+			<div className={style.main_block}>
+				<h2>Search</h2>
+				<div className={style.input_container}>
+					<input
+						type="text"
+						placeholder="Deadpool, Shot Caller..."
+						className={style.input_section}
+					/>
+					<Icon name="search" />
+				</div>
+
+				<section className={style.search_section}>
+					<div className={style.search_item}>
+						<p className={style.item_name}>Inglourious Basterds</p>
+						<span className={style.item_details}>2017</span>
+					</div>
+					<div className={style.search_item}>
+						<p className={style.item_name}>Inglourious Basterds</p>
+						<span className={style.item_details}>2017</span>
+					</div>
+					<div className={style.search_item}>
+						<p className={style.item_name}>
+							Inglourious Basterds Basterds Basterds
+						</p>
+						<span className={style.item_details}>2017</span>
+					</div>
+					<div className={style.search_item}>
+						<p className={style.item_name}>Django unchained</p>
+						<span className={style.item_details}>2017</span>
+					</div>
+					<div className={style.search_item}>
+						<p className={style.item_name}>Basterds Inglourious Basterds</p>
+						<span className={style.item_details}>2017</span>
+					</div>
+					<div className={style.search_item}>
+						<p className={style.item_name}>Inglourious Basterds</p>
+						<span className={style.item_details}>2017</span>
+					</div>
+					<div className={style.search_item}>
+						<p className={style.item_name}>Inglourious Basterds</p>
+						<span className={style.item_details}>2017</span>
+					</div>
+					<div className={style.search_item}>
+						<p className={style.item_name}>Inglourious Basterds</p>
+						<span className={style.item_details}>2017</span>
+					</div>
+					<div className={style.search_item}>
+						<p className={style.item_name}>Inglourious Basterds</p>
+						<span className={style.item_details}>2017</span>
+					</div>
+					<div className={style.search_item}>
+						<p className={style.item_name}>Inglourious Basterds</p>
+						<span className={style.item_details}>2017</span>
+					</div>
+				</section>
+			</div>
 		</div>
 	);
 };
