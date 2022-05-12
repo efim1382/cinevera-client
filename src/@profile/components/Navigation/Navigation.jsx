a3c28147b0f5a060d9c9b31f1c9a71d1651fe314
kottbart <yuriikorbat@gmail.com>
2022-05-12 19:46:13 +0300

    fixed profile page and add make mobile version

--- src/@profile/components/Navigation/Navigation.jsx
+++ src/@profile/components/Navigation/Navigation.jsx
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Button from "components/BasicButton";
+import { NavLink } from "react-router-dom";
+import classnames from "classnames/bind";
 import style from "./style.css";
+
+const cx = classnames.bind(style);
 
 const Navigation = () => {
 	return (
-		<nav className={style.navigation}>
-			Navigation
-		</nav>
+		<Fragment>
+			<div className={style.information}>
+				<div className={style.profile_section}>
+					<div className={style.avatar} />
+
+					<div className={style.user_details}>
+						<h3 className={style.name}>Yurii Korbat</h3>
+
+						<span className={style.mail}>romahochetvtro@gmail.com</span>
+						<Button
+							theme="accent"
+							appearance="fill"
+							text="Edit Profile"
+							className={style.edit_profile}
+						/>
+					</div>
+				</div>
+			</div>
+
+			<div className={style.navigation_block}>
+				<div className="container">
+					<nav className={style.navigation}>
+						<NavLink to="/movies/details/queens-gambit/" className={cx("link_button", "_is-active")}>Watch Later</NavLink>
+						<NavLink to="/movies/details/queens-gambit/" className={style.link_button}>Best 2022</NavLink>
+						<NavLink to="/movies/details/queens-gambit/" className={style.link_button}>TV Series</NavLink>
+						<NavLink to="/movies/details/queens-gambit/" className={style.link_button}>Collections</NavLink>
+					</nav>
+				</div>
+			</div>
+		</Fragment>
 	);
 };
 
--- src/@profile/containers/RecentMovies/RecentMovies.jsx
+++ src/@profile/containers/RecentMovies/RecentMovies.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import MovieCard from "components/MovieCard";
 import style from "./style.css";
 
 const RecentMovies = () => {
 	return (
 		<div className={style.recent_movies}>
-			Recent
+			<div className="container">
+				<div className={style.movies_list}>
+					<MovieCard id={3} className={style.card} />
+					<MovieCard id={3} className={style.card} />
+					<MovieCard id={3} className={style.card} />
+					<MovieCard id={3} className={style.card} />
+					<MovieCard id={3} className={style.card} />
+					<MovieCard id={3} className={style.card} />
+					<MovieCard id={3} className={style.card} />
+					<MovieCard id={3} className={style.card} />
+					<MovieCard id={3} className={style.card} />
+				</div>
+			</div>
 		</div>
 	);
 };
