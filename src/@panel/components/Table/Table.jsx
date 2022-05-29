3395366eac731d54b23054ed53e8f097df346bf7
kottbart <yuriikorbat@gmail.com>
2022-05-30 02:55:49 +0300

    added styles to components in table

--- src/@panel/components/Table/Table.jsx
+++ src/@panel/components/Table/Table.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import classnames from "classnames/bind";
 import Checkbox from "./Checkbox";
+import Icon from "components/Icon";
 import HeadingCell from "./HeadingCell";
 import Cell from "./Cell";
 import style from "./style.css";
@@ -13,8 +14,9 @@ const Table = ({ className }) => {
 		<div className={cx("table", className)}>
 			<div className={style.headings}>
 				<Checkbox />
-				<HeadingCell width={150} text="Name" />
-				<HeadingCell width={150} text="Year" />
+				<HeadingCell width={200} text="Name" />
+				<HeadingCell width={80} text="Year" />
+				<div className={style.fake_button} />
 			</div>
 
 			{Array.from(Array(20).keys()).map((index) => {
@@ -22,13 +24,17 @@ const Table = ({ className }) => {
 					<div key={index} className={style.row}>
 						<Checkbox />
 
-						<Cell width={150}>
+						<Cell width={200}>
 							<p>Name</p>
 						</Cell>
 
-						<Cell width={150}>
+						<Cell width={80}>
 							<p>Name</p>
 						</Cell>
+
+						<button type="button" className={style.delete_button}>
+							<Icon name="delete" />
+						</button>
 					</div>
 				);
 			})}
